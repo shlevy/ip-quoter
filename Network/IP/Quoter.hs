f78b6c86c873fef3ccdea70743c177326933a189
Shea Levy <shea@shealevy.com>
2015-06-10 15:09:20 -0400

    Implement ipv4 parsing

--- Network/IP/Quoter.hs
+++ Network/IP/Quoter.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Network.IP.Quoter (ip) where
 
+import Data.Char ( isDigit )
+import Data.Bits ( (.|.), unsafeShiftL )
+import Data.List.Split ( splitOn )
+import Network.Socket ( HostAddress )
 import Language.Haskell.TH.Quote ( QuasiQuoter( .. ) )
-import Language.Haskell.TH.Syntax ( Q, Exp )
+import Language.Haskell.TH.Syntax ( Q, Exp( .. ), Lit( .. ), Type ( .. ) )
 
 ip :: QuasiQuoter
 ip = QuasiQuoter
@@ -12,5 +17,32 @@ ip = QuasiQuoter
   , quoteExp = parseIP
   }
 
+parseDigit :: Char -> Q Integer
+parseDigit c
+  | isDigit c = return . fromIntegral $ (fromEnum c - fromEnum '0')
+  | otherwise = fail "Non-digit in IP address"
+
+parseSegment' :: String -> Integer -> Q Integer
+parseSegment' [] total = return total
+parseSegment' (digit : rest) total
+  | total <= 25 = do
+      next <- parseDigit digit
+      if (total == 0) && (next == 0) && (not (null rest))
+        then fail "Leading zero in IP address segment"
+        else parseSegment' rest (total * 10 + next)
+  | otherwise = fail "IP address segment too big"
+
+parseSegment :: String -> Q Integer
+parseSegment [] = fail "Empty IP address segment"
+parseSegment str = parseSegment' str 0
+
 parseIP :: String -> Q Exp
-parseIP _ = error "Not yet implemented"
+parseIP s = case (splitOn "." s) of
+  l@[ first, second, third, fourth ] -> do
+      bytes <- mapM (fmap (LitE . IntegerL) . parseSegment) l
+      return $ SigE (foldl1 shiftAndOr bytes) (ConT ''HostAddress)
+    where
+      shiftAndOr acc exp = AppE (AppE (VarE '(.|.)) shifted) exp
+        where
+          shifted = AppE (AppE (VarE 'unsafeShiftL) acc) (LitE $ IntegerL 8)
+  _ -> fail "IP address doesn't contain four segments"
--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, network, stdenv, tasty, tasty-hunit
+{ mkDerivation, base, network, split, stdenv, tasty, tasty-hunit
 , template-haskell
 }:
 mkDerivation {
   pname = "ip-quoter";
   version = "1.0.0.0";
   src = ./.;
-  buildDepends = [ base template-haskell ];
+  buildDepends = [ base network split template-haskell ];
   testDepends = [ base network tasty tasty-hunit ];
   homepage = "https://github.com/shlevy/ip-quoter";
   description = "Quasiquoter for IP addresses";
